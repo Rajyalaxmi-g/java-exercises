d0d418ce0fc135d5a3202c7752743d921b2af2b0
Rajyalaxmi <gaddala.rajyalaxmi14@gmail.com>
2018-11-02 22:18:52 -0500

    first program

---
+++ src/org/launchcode/java/demos/java4python/sample.java
@@ -0,0 +1,7 @@
+package org.launchcode.java.demos.java4python;
+
+public class sample {
+    public static void main(String[] args) {
+        System.out.println("welcome to java programming");
+    }
+}
